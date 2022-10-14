f6d2ba06fea9c0bc9e727a83b3485dfacfafb3f5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-10-14 18:05:18 +0200

    Switch to wyrihaximus/metrics for metrics

--- src/bootstrap_include.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace ReactInspector;
-
-// @codeCoverageIgnoreStart
-if (!\defined('ReactInspector\BOOTSTRAPPED')) {
-    require __DIR__ . '/bootstrap.php';
-}
-// @codeCoverageIgnoreEnd
